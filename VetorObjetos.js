5f5f0b6c9b09b730c74cbac4fb77bb4581247a8e
LENNON CINTRA GARCIA <1090481721028@fatec.local>
2018-05-08 10:13:00 -0300

    novas areas vetor objeto

--- VetorObjetos.js
+++ VetorObjetos.js
@@ -11,9 +11,39 @@ function cadastra() {
     alert("Cadastrado com sucesso")
 }
 function lista() {
+    //lista
     var saida = "";
+    //aprovado exame ou reprovado
     for (var i = 0; i < vetor.length; i++) {
-        saida = saida + vetor[i].nome + " " + vetor[i].media + "<br>";
+        saida = saida + vetor[i].nome + " " + vetor[i].media;
+
+            if (vetor[i].media >= 6){
+                saida = saida + " - Aprovado";
+            }
+            else if (vetor[i].media >= 3){
+                saida = saida + " - Exame";
+            } 
+            else saida = saida + " - Reprovado";
+            saida = saida + "<br/>"
     }
+        //saida dos dados pro html
     document.getElementById("lista").innerHTML = saida;
+}
+
+function media() {
+    var maior = vetor[0].media;
+    var menor = vetor[0].media;
+    var nome = vetor[0].nome;
+    var nomemenor = vetor[0].nome;
+    for (var i = 0; i < vetor.length; i++) {
+        if (vetor[i].media > maior) {
+            maior = vetor[i].media
+            nome = vetor[i].nome
+        }
+        if (vetor[i].media < menor) {
+            menor = vetor[i].media
+            nomemenor = vetor[i].nome
+        }
+    }
+        document.getElementById("resultado").innerHTML = "O Aluno " + nome + " Teve a Maior nota " + maior + "\n" + "O Aluno " + nomemenor + " Teve a Menor nota " + menor;
 }
